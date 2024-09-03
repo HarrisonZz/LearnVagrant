6376a7393b50babaf27f366922d6617e05c38aef
Harrison <squad1033@gmail.com>
2024-09-04 01:50:19 +0800

    modify private ip to match VMWare default setting

--- centos/provision.sh
+++ centos/provision.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo yum update -y
-sudo yum install httpd zip unzip vim wget -y
+sudo yum install sudo httpd zip unzip vim wget -y
 
 sudo systemctl start httpd
 sudo systemctl enable httpd
