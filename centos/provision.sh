1a28a9bfb8dbad91143be0e5ab93b145571b5f45
Harrison <squad1033@gmail.com>
2024-09-06 01:51:41 +0800

    Update provision.sh of centos VM for build up a website automatically

--- centos/provision.sh
+++ centos/provision.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
+TARGET="https://www.tooplate.com/zip-templates/2107_new_spot.zip"
+ZIP_FILE="website.zip"
+TEMP_PATH="/tmp/website"
+
 sudo yum update -y
 sudo yum install sudo httpd zip unzip vim wget -y
+if systemctl is-enabled httpd &> /dev/null; then
+    echo "httpd have been started"
+else
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+fi
 
-sudo systemctl start httpd
-sudo systemctl enable httpd
+
+if [ -n "${TARGET}" ]; then
+  sudo wget -O ${ZIP_FILE} ${TARGET}
+  sudo unzip -d ${TEMP_PATH} ${ZIP_FILE}
+  sudo rm -rf /var/www/html/* && sudo find ${TEMP_PATH} -mindepth 2 -exec mv -t /var/www/html/ {} + > /dev/null 2>&1
+  sudo rm -rf ${TEMP_PATH}
+else
+  echo "No website update"
+fi
